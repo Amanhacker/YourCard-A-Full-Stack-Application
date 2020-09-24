6cb1e3ed61a2d9195a35a6faffb62b3f1d9f794f
Anuj Sharma <vasusharma.in@gmail.com>
2020-09-25 02:51:11 +0530

    Chart display acc to paymentlist

--- FrontEnd/src/app/services/payment.service.ts
+++ FrontEnd/src/app/services/payment.service.ts
@@ -1,18 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PaymentService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   addUserPaymentData(payment: any) {
-    return this.httpClient.post("http://localhost:8765/paymentservice/addPayment", payment);
+    return this.httpClient.post(
+      "http://localhost:8765/paymentservice/addPayment",
+      payment
+    );
   }
 
   getAllPayments(userId) {
-    return this.httpClient.get("http://localhost:8765/paymentservice/allPayments/"+userId);
+    return this.httpClient.get(
+      "http://localhost:8765/paymentservice/allPayments/" + userId
+    );
   }
 }
